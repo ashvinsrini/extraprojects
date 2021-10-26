54650e2ccdd1c3129ffddd9c59d55ecdf8bf4b95
ashvinsrini <ashvinpesit@gmail.com>
2021-10-26 22:40:14 +0530

    adding expected SARSA and reorganising the folder structure

--- Qlearning/Q_SARSA_ExpectedSARSA/main_1a.py
+++ Qlearning/Q_SARSA_ExpectedSARSA/main_1a.py
@@ -2,13 +2,19 @@ import gym
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
-from DQN import Qlearning,SARSA
+import pandas as pd
+from qlearn_SARSA import Qlearning,SARSA, Expected_SARSA
 env = gym.make("MountainCar-v0")
+import random
+import os
 
+random.seed(10)
+np.random.seed(10)
 lr = 0.1
 gamma = 0.9
 episodes = 2_000
 SHOW_EVERY = 100
+method = 'Q'
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low)/discrete_os_win_size
     return tuple(discrete_state.astype(np.int))
@@ -23,9 +29,13 @@ end_epsilon_decay = episodes//2
 print(discrete_os_win_size)
 
 q_table = np.random.uniform(low = -2, high = 0, size = (DISCRETE_OS_SIZE + [env.action_space.n]))
-qlearn = Qlearning(q_table, lr, gamma)
-#qlearn = SARSA(q_table, lr, gamma)
-
+if method == 'Q':
+    qlearn = Qlearning(q_table, lr, gamma)
+elif method == 'SARSA':
+    qlearn = SARSA(q_table, lr, gamma)
+else:
+    qlearn = Expected_SARSA(q_table, lr, gamma)
+all_episodes_rewards = []
 dict_ep_rewards = {'ep' : [], 'mean' : [], 'min' : [], 'max' : []}
 
 for episode in range(episodes):
@@ -55,11 +65,12 @@ for episode in range(episodes):
         ep_r+=reward
         new_discrete_state = get_discrete_state(new_state)
         if (episode>500) and (episode%10==0):
-            env.render()
+            #env.render()
             pass
-
-        qlearn.max_q(new_discrete_state)
-        #qlearn.max_q(new_discrete_state, pr, env, epsilon)
+        if method == 'Q':
+            qlearn.max_q(new_discrete_state)
+        else:
+            qlearn.max_q(new_discrete_state, pr, env, epsilon)
         qlearn.update_new_q(discrete_state, action, reward, done, new_state, final_pos = env.goal_position)
         if new_state[0] >= env.goal_position:
             print('reward:', reward)
@@ -69,6 +80,7 @@ for episode in range(episodes):
         step_per_epi+=1
     #pdb.set_trace()
     per_ep_rewards.append(ep_r)
+    all_episodes_rewards.append(ep_r)
     print('ep_r',ep_r)
     t+=1
     if len(per_ep_rewards) == SHOW_EVERY:
@@ -77,16 +89,34 @@ for episode in range(episodes):
         dict_ep_rewards['mean'].append(mean_reward)
         dict_ep_rewards['min'].append(np.min(per_ep_rewards))
         dict_ep_rewards['max'].append(np.max(per_ep_rewards))
+
         print("Episode:{}, mean:{}, min:{}, max:{}".format(episode, mean_reward,np.min(per_ep_rewards),np.max(per_ep_rewards)))
         #pdb.set_trace()
 
     #if end_epsilon_decay >= episode >= start_epsilon_decay:
         #epsilon -= epsilon_decay_value
 env.close()
-
-plt.figure()
+'''
+plt.figure(1)
 plt.plot(dict_ep_rewards['ep'], dict_ep_rewards['mean'], label = 'mean')
 plt.plot(dict_ep_rewards['ep'], dict_ep_rewards['min'], label = 'min')
 plt.plot(dict_ep_rewards['ep'], dict_ep_rewards['max'], label = 'max')
 plt.legend()
 plt.show()
+'''
+if not os.path.exists('./results'):
+    os.makedirs('.results')
+save_pth = os.path.join(os.getcwd(),'results')
+
+plt.figure(1)
+plt.plot([_ for _ in range(len(all_episodes_rewards))], all_episodes_rewards)
+plt.savefig(os.path.join(save_pth, 'all_episodes_rewards_{}.png'.format(method)))
+plt.show()
+
+
+series = pd.Series(all_episodes_rewards).rolling(window = 50).mean()
+plt.figure(2)
+plt.plot(series)
+plt.savefig(os.path.join(save_pth, 'all_episodes_rewards_rollingmean_{}.png'.format(method)))
+plt.show()
+
--- Qlearning/Q_SARSA_ExpectedSARSA/qlearn_SARSA.py
+++ Qlearning/Q_SARSA_ExpectedSARSA/qlearn_SARSA.py
@@ -48,3 +48,30 @@ class SARSA:
         elif new_state[0] > final_pos:
             self.q_table[discrete_state + (action,)] = 0
         # self.new_state = new_state
+
+
+class Expected_SARSA:
+    def __init__(self, q_table, lr, gamma):
+        self.q_table = q_table
+        self.lr = lr
+        self.gamma = gamma
+        pass
+
+    def max_q(self, new_discrete_state, pr, env, epsilon):
+        all_actions = [0,1,2]
+        action = np.argmax(self.q_table[new_discrete_state])
+        other_actions = np.array([a for a in all_actions if a != action])
+
+        self.max_future_q = 0.8*np.max(self.q_table[new_discrete_state]) + \
+                            0.1*self.q_table[new_discrete_state + (other_actions[0],)] + \
+                            0.1*self.q_table[new_discrete_state + (other_actions[1],)]
+
+
+    def update_new_q(self, discrete_state, action, reward, done, new_state, final_pos):
+        if not done:
+            current_q = self.q_table[discrete_state + (action,)]
+            new_q = (1 - self.lr) * current_q + self.lr * (reward + self.gamma * self.max_future_q)
+            self.q_table[discrete_state + (action,)] = new_q
+        elif new_state[0] > final_pos:
+            self.q_table[discrete_state + (action,)] = 0
+        # self.new_state = new_state
